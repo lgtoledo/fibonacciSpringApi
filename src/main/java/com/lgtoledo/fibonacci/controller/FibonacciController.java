12697f122b45d8003fb38198c7309670be301ca7
Leandro Toledo <lgtoledo@outlook.com>
2022-07-10 02:49:25 -0300

    Se usa capa de servicio y se agrega reset de BD

--- src/main/java/com/lgtoledo/fibonacci/controller/FibonacciController.java
+++ src/main/java/com/lgtoledo/fibonacci/controller/FibonacciController.java
@@ -1,17 +1,17 @@
 package com.lgtoledo.fibonacci.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lgtoledo.fibonacci.data.model.Term;
-import com.lgtoledo.fibonacci.data.repository.TermRepository;
+import com.lgtoledo.fibonacci.service.TermService;
 
 @RestController
 public class FibonacciController {
-  long[] fibonacci = new long[93];
   
-  TermRepository repository;
+  @Autowired
+  TermService termService;
 
   @RequestMapping("/")
   public String Index() {
@@ -24,30 +24,17 @@ public class FibonacciController {
 
     if (n > 92) return "Error 2: Ingrese un valor menor o igual a 92 para evitar overflow del tipo -long-";
     
-    repository.save(new Term(0, 0));
-
-    return String.valueOf(calcFibonacci(n));
+    return String.valueOf(termService.getTerm(n).getValue());
   }
   
-  private long calcFibonacci(int n) {
-    // Verifico si el número es 0 o 1
-    if (n == 0) {
-      return 0;
-    }
-    if (n == 1) {
-      return 1;
-    }
-
-    // Verifico si ya calculé el número
-    if (n <= fibonacci.length) {
-      return fibonacci[n];
-    }else{
-      // Calculo el número
-      fibonacci[n] = calcFibonacci(n-1) + calcFibonacci(n-2);
-      return fibonacci[n];
+  @RequestMapping("/fibonacci/reset")
+  public String reset() {
+    try {
+      termService.reset();
+      return "Contenido de la Base de datos borrado correctamente";
+    } catch (Exception e) {
+      return "Error al limpiar la base de datos:" + "\n" + e.getMessage();
     }
   }
-
-  
 
 }
