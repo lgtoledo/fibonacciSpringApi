7f568d2be4dd74e3bed618240820449a3e117f95
Leandro Toledo <lgtoledo@outlook.com>
2022-07-11 02:42:38 -0300

    implemento excpeciones y retorno de jsons

--- src/main/java/com/lgtoledo/fibonacci/controller/FibonacciController.java
+++ src/main/java/com/lgtoledo/fibonacci/controller/FibonacciController.java
@@ -1,13 +1,17 @@
 package com.lgtoledo.fibonacci.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.lgtoledo.fibonacci.data.model.Term;
 import com.lgtoledo.fibonacci.service.TermService;
+import com.nimbusds.jose.shaded.json.JSONObject;
 
 @RestController
 @RequestMapping("/fibonacci")
@@ -16,27 +20,32 @@ public class FibonacciController {
   @Autowired
   TermService termService;
 
-  @RequestMapping("/")
-  public String index() {
-    return "Hola";
-  }
-
-  @GetMapping("/{n}")
-  public Term fibonacci(@PathVariable int n) {
-    //if (n < 0) return "Error 1: Ingrese un valor mayor o igual a cero";
-
-    //if (n > 92) return "Error 2: Ingrese un valor menor o igual a 92 para evitar overflow del tipo -long-";
+  @GetMapping(value="/{n}", produces="application/json")
+  public Term fibonacci(@PathVariable int n) throws Exception {
+   
+    if (n < 0) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Ingrese un valor mayor o igual a cero");
+    if (n > 92) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Ingrese un valor menor o igual a 92 para evitar overflow del tipo -long-");
     
-    return this.termService.getTerm(n);
+    try {
+      Term t = this.termService.getTerm(n);
+      return t;
+    }catch (Exception e) {
+      throw new ResponseStatusException(
+           HttpStatus.INTERNAL_SERVER_ERROR, "Error al obtener n-term de fibonacci.", e);
+    }
   }
   
-  @GetMapping("/reset")
+  @GetMapping(value="/reset", produces="application/json")
   public String reset() {
     try {
       this.termService.reset();
-      return "Contenido de la Base de datos borrado correctamente";
-    } catch (Exception e) {
-      return "Error al limpiar la base de datos:" + "\n" + e.getMessage();
+      JSONObject entity = new JSONObject();
+      entity.put("message", "Contenido de la Base de datos borrado correctamente");
+      return entity.toJSONString();
+      
+    }catch (Exception e) {
+      throw new ResponseStatusException(
+           HttpStatus.INTERNAL_SERVER_ERROR, "Error al limpiar la base de datos.", e);
     }
   }
 
