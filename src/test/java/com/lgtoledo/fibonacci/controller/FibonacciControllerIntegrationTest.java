a6a88376e95248959744eb7e6e0375dba1d0bc2f
Leandro Toledo <lgtoledo@outlook.com>
2022-07-11 16:15:09 -0300

    Test de integración para endpoint fibonacci/reset

--- src/test/java/com/lgtoledo/fibonacci/controller/FibonacciControllerIntegrationTest.java
+++ src/test/java/com/lgtoledo/fibonacci/controller/FibonacciControllerIntegrationTest.java
@@ -1,14 +1,9 @@
 package com.lgtoledo.fibonacci.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
-import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -22,7 +17,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -31,22 +25,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.lgtoledo.fibonacci.FibonacciApplication;
-import com.lgtoledo.fibonacci.service.TermService;
+import com.lgtoledo.fibonacci.data.repository.TermRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = FibonacciApplication.class)
 @AutoConfigureMockMvc
 @EnableAutoConfiguration
-//@TestPropertySource(locations = "classpath:application-integrationtest.properties")
-//@AutoConfigureTestDatabase
 public class FibonacciControllerIntegrationTest {
   
   @Autowired
   private MockMvc mvc;
 
   @Autowired
-  private TermService termService;
-
+  private TermRepository termRepository;
   
   @Test
 	public void givenNTerm_whenGetNTerm_thenStatus200() throws Exception {
@@ -59,10 +50,19 @@ public class FibonacciControllerIntegrationTest {
     .andExpect(jsonPath("value", is(8)));
   }
 
-
   @Test
-  public void testReset() {
+  public void testResetEndpoint() throws Exception{
 
+    long count = termRepository.count();
+    assertThat(count).isEqualTo(7);
+
+    mvc.perform(get("/fibonacci/reset").contentType(MediaType.APPLICATION_JSON))
+    .andDo(print())
+    .andExpect(status().isOk())
+    .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+
+    long countAfterReset = termRepository.count();
+    assertThat(countAfterReset).isEqualTo(0);
   }
 
   @BeforeAll
