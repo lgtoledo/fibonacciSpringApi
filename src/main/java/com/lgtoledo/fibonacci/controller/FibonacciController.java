6eaefa3c1f2c6c956c95ea4bd0afbbc5a0b44f40
Leandro Toledo <lgtoledo@outlook.com>
2022-07-10 23:48:50 -0300

    Controller retorna objeto Term en lugar de string

--- src/main/java/com/lgtoledo/fibonacci/controller/FibonacciController.java
+++ src/main/java/com/lgtoledo/fibonacci/controller/FibonacciController.java
@@ -1,36 +1,39 @@
 package com.lgtoledo.fibonacci.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lgtoledo.fibonacci.data.model.Term;
 import com.lgtoledo.fibonacci.service.TermService;
 
 @RestController
+@RequestMapping("/fibonacci")
 public class FibonacciController {
   
   @Autowired
   TermService termService;
 
   @RequestMapping("/")
-  public String Index() {
+  public String index() {
     return "Hola";
   }
 
-  @RequestMapping("/fibonacci/{n}")
-  public String fibonacci(@PathVariable int n) {
-    if (n < 0) return "Error 1: Ingrese un valor mayor o igual a cero";
+  @GetMapping("/{n}")
+  public Term fibonacci(@PathVariable int n) {
+    //if (n < 0) return "Error 1: Ingrese un valor mayor o igual a cero";
 
-    if (n > 92) return "Error 2: Ingrese un valor menor o igual a 92 para evitar overflow del tipo -long-";
+    //if (n > 92) return "Error 2: Ingrese un valor menor o igual a 92 para evitar overflow del tipo -long-";
     
-    return String.valueOf(termService.getTerm(n).getValue());
+    return this.termService.getTerm(n);
   }
   
-  @RequestMapping("/fibonacci/reset")
+  @GetMapping("/reset")
   public String reset() {
     try {
-      termService.reset();
+      this.termService.reset();
       return "Contenido de la Base de datos borrado correctamente";
     } catch (Exception e) {
       return "Error al limpiar la base de datos:" + "\n" + e.getMessage();
