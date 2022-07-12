161844358e5aae142d485844c8976bf8cb129b4b
Leandro Toledo <lgtoledo@outlook.com>
2022-07-12 17:46:07 -0300

    limpieza de imports no utilizados

--- src/test/java/com/lgtoledo/fibonacci/controller/FibonacciControllerIntegrationTest.java
+++ src/test/java/com/lgtoledo/fibonacci/controller/FibonacciControllerIntegrationTest.java
@@ -21,7 +21,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
