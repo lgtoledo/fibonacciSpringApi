22a3f4b198b2e44b2914c948af538698e6736b46
Leandro Toledo <lgtoledo@outlook.com>
2022-07-12 21:27:57 -0300

    Query para obtener métrica de 3 más solicitados

--- src/main/java/com/lgtoledo/fibonacci/data/repository/MetricRepository.java
+++ src/main/java/com/lgtoledo/fibonacci/data/repository/MetricRepository.java
@@ -2,6 +2,7 @@ package com.lgtoledo.fibonacci.data.repository;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.lgtoledo.fibonacci.data.model.Metric;
@@ -10,9 +11,11 @@ public interface MetricRepository extends CrudRepository<Metric, Long> {
 
   public Iterable<Metric> findByTimestampBetween(LocalDateTime start, LocalDateTime end);
 
-  public Iterable<Metric> findBynTerm(int nTerm);
+  public Iterable<Metric> findBynTerm(long nTerm);
 
-  public Iterable<Metric> findBynTermAndTimestampBetween(int nTerm, LocalDateTime start, LocalDateTime end);
+  public Iterable<Metric> findBynTermAndTimestampBetween(long nTerm, LocalDateTime start, LocalDateTime end);
 
+  @Query(value = "SELECT n_term, COUNT(n_term) AS freq FROM Metric GROUP BY n_term ORDER BY freq DESC LIMIT 3", nativeQuery = true)
+  public Iterable<Object> find3MostFrequent();
 }
 
